0acb68a518cd04f5abd2762aaf8d9f90dd5efc1d
rahulgehlot <rahulgehlot9004@gmail.com>
2023-08-21 19:42:35 +0530

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(2 + 2) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3)  //code readability should be high
+
+console.log("Rahul")
+
+let name = "Rahul"  
+let age = 3     
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => uniqueness
+
+
+// object
+
+
+console.log(typeof null)    //object
+console.log(typeof undefined)   //undefined
