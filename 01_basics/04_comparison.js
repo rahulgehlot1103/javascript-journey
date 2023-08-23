ee7988b315e9dc487f2767b88719add2a87b4918
rahulgehlot <rahulgehlot9004@gmail.com>
2023-08-23 13:58:32 +0530

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);   
+// 2 is converted in a number
+// console.log("02" > 1);
+
+
+/* Avoid in most of the cases
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+*/
+
+
+
+// === strict check, datatype b check krtaa h
+console.log("2" === 2);
+
