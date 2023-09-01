9ed5a3706cd03be6b0db2890bcc07fd2c4274739
rahulgehlot <rahulgehlot9004@gmail.com>
2023-09-01 19:45:50 +0530

    stack and heap done

--- 01_basics/datatypes_summery.js
+++ 01_basics/datatypes_summery.js
@@ -1,7 +1,7 @@
 // Primitive 
 
 // 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
-
+/*
 
 const value = 100
 const average = 100.3
@@ -42,4 +42,33 @@ console.log(typeof id);
 console.log(typeof heros);
 console.log(typeof userDetails);
 console.log(typeof myFunction);
+
+*/
+
+//**************************************************** */
+
+// Stack(Primitive), Heap(Non-primitive)
+// Stack gives the copy 
+// Heap gives the reference
+
+let myName = "Rahul";
+
+let anotherName = myName;
+anotherName = "Gehlot"
+console.log(anotherName);
+console.log(myName);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@oksbi"
+}
+
+let userTwo = userOne;
+
+
+userTwo.email = "rahul@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
 
