001afb21803af937d53b40f19157e66f2cb0bb64
rahulgehlot <rahulgehlot9004@gmail.com>
2023-09-01 19:47:01 +0530

    conversion operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -42,11 +42,10 @@ console.log(2%3);   //remainder
 
 */
 
-let str1="hello"
-let str2 = " rahul"
+let str1 = "hello";
+let str2 = " rahul";
 
-
-let str3 = str1 + str2
+let str3 = str1 + str2;
 console.log(str3);
 
 // console.log("1" + 2);
@@ -58,10 +57,10 @@ console.log(str3);
 // console.log(+true);
 // console.log(+"")
 
-let num1, num2, num3
+let num1, num2, num3;
 
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2;
 
-let gameCounter = 100
-++gameCounter 
+let gameCounter = 100;
+++gameCounter;
 console.log(gameCounter);
